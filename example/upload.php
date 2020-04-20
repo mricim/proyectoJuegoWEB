e2befa350aa459c6beb3910c8a021db2fbae6cf0
MrICIM <ericcasanova.m@gmail.com>
2020-04-20 09:25:43 +0200

    upload test 5

--- example/upload.php
+++ example/upload.php
@@ -1,5 +1,5 @@
 <?php
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 echo $actual_link;
 $target_dir = "uploads/";
 echo $target_dir;
