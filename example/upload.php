40702396120bf8e0d7335591e7d1534ddb461241
MrICIM <ericcasanova.m@gmail.com>
2020-04-20 09:29:20 +0200

    upload test 7

--- example/upload.php
+++ example/upload.php
@@ -1,8 +1,8 @@
 <?php
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-echo $actual_link;
 $target_dir = $actual_link."/uploads/";
-echo $target_dir;
+echo $target_dir;//PRINT TEST
+
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
