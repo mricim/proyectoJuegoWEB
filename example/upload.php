06bf1703e4e3019a021db74b64d3c9ae83ce5e95
MrICIM <ericcasanova.m@gmail.com>
2020-04-20 10:12:47 +0200

    upload test 11

--- example/upload.php
+++ example/upload.php
@@ -1,7 +1,7 @@
 <?php
 //$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 $target_dir = "/var/www/html/armegis.tk/uploads/";
-echo $target_dir;//PRINT TEST
+//echo $target_dir;//PRINT TEST
 
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -10,7 +10,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "File is an image - " . $check["mime"] . ".<br>";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -19,7 +19,7 @@ if(isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "Sorry, file already exists.<br>";
     $uploadOk = 0;
 }
 // Check file size
@@ -30,7 +30,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -41,7 +41,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Sorry, there was an error uploading your file.<br>";
     }
 }
 ?>
