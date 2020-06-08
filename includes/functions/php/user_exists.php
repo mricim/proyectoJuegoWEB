f2468869ba1b65cf6eb964d637e93ddedd87e285
unknown <monicasegarra3@gmail.com>
2020-06-09 01:50:00 +0200

    modificació user_exists.php

--- includes/functions/php/user_exists.php
+++ includes/functions/php/user_exists.php
@@ -7,8 +7,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once('db_connection.php');
-include('encrypt_password.php');
+include_once 'db_connection.php';
+include 'encrypt_password.php';
 
 		//Comprueba si existe el usuario
 
