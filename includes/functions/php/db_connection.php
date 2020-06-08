9114e43cf1ce19364b032bdbaa8c7292b43be730
unknown <monicasegarra3@gmail.com>
2020-06-08 18:37:53 +0200

    modificació insertUser.php i creació db_connection.php

---
+++ includes/functions/php/db_connection.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+//PARA VER LOS ERRORES
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function conectar_db(){
+		// conexion a Servidor
+		$host = "localhost";#nombre del host
+		$user = "user";#Usuario
+		$pass = "user12345!Ab";#contraseña
+		$db_name = "armegis";#Nombre de la base de datos
+		$conexion = mysqli_connect($host, $user, $pass);#Envio a la base de datos (Todas las variables antes creadas)
+		$conexion->query("SET NAMES 'utf8'");#Le decimos en que charset hay que conectarse
+
+		if( !$conexion ){ //Si la conexión no se ha logrado
+			echo "No se puede conectar a la BD";
+			exit;
+		} else {
+			//seleccion de base de datos(Aqui ya se a establecido la conecxion continua) y iniciamos a decirle a que base de datos se tiene que conectar
+			$select_db = mysqli_select_db($conexion,$db_name);
+			if( !$select_db ){#La base de datos no existe o hay algun problema
+				echo "Imposible seleccionar BD!!";
+				exit;
+			}
+			else{#TODO CORRECTO
+			echo "Conectado a la BD";
+				return $conexion;
+			}
+		}
+	}
+
+	?>
+	</body>
+	</html>
--- includes/functions/php/insertUser.php
+++ includes/functions/php/insertUser.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>My first PHP page</h1>
 <?php
 //PARA VER LOS ERRORES
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function conectar_db(){
-		// conexion a Servidor
-		$host = "localhost";#nombre del host
-		$user = "user";#Usuario
-		$pass = "user12345!Ab";#contraseña
-		$db_name = "armegis";#Nombre de la base de datos
-		$conexion = mysqli_connect($host, $user, $pass);#Envio a la base de datos (Todas las variables antes creadas)
-		$conexion->query("SET NAMES 'utf8'");#Le decimos en que charset hay que conectarse
-
-		if( !$conexion ){ //Si la conexión no se ha logrado
-			echo "No se puede conectar a la BD";
-			exit;
-		} else {
-			//seleccion de base de datos(Aqui ya se a establecido la conecxion continua) y iniciamos a decirle a que base de datos se tiene que conectar
-			$select_db = mysqli_select_db($conexion,$db_name);
-			if( !$select_db ){#La base de datos no existe o hay algun problema
-				echo "Imposible seleccionar BD!!";
-				exit;
-			}
-			else{#TODO CORRECTO
-			echo "Conectado a la BD";
-				return $conexion;
-			}
-		}
-	}
-
-
-
+include("db_connection.php");
 
 
 		//$consultar_mensaje = "INSERT INTO usuarios (nombre,numero,email,reg_date) VALUES ( 'nombre', 15, '".addslashes('correoelectronico@gmail.com')."', '2017-07-23')";
 		//guardar usuarios
 		$links = conectar_db();
-		$consultar_mensaje = "INSERT INTO users (name,email,password,date_register,last_conexion) VALUES ( 'nombre', 'correoelectronico@gmail.com', 'kjdhdf',now(),now())";
+
+		//Valores del formulario de registro
+		if ($_POST) {
+		    $userName = "";
+        	$userPass = "";
+        	$userMail = "";
+
+        	if (isset($_POST['name'])) {
+        	    $userName = $_POST['name'];
+        	}
+        	if (isset($_POST['password'])) {
+                $userPass = $_POST['password'];
+            }
+            if (isset($_POST['email'])) {
+                $userMail = $_POST['email'];
+            }
+
+		}
+
+
+		$consultar_mensaje = "INSERT INTO users (name,email,password,date_register,last_conexion)
+		VALUES ( '$userName', '$userMail', '$userPass',now(),now())";
 		$result_mensaje = mysqli_query($links, $consultar_mensaje);
 
 		if (isset($result_mensaje) == FALSE) {
