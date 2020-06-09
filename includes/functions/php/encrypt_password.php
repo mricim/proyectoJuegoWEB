466eca8d528f0f01fd9bc3fd090789ee7ad33a24
Monica <monicasegarra3@gmail.com>
2020-06-09 03:42:22 +0200

    Arreglo variables encrypt_password.php

--- includes/functions/php/encrypt_password.php
+++ includes/functions/php/encrypt_password.php
@@ -10,12 +10,12 @@ $method = 'aes-256-cbc';
 $iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");
 
  //Encripta
- $encrypt = function ($valor) use ($method, $clave, $iv) {
+ $encrypt = function ($valor, $method, $clave, $iv) {
      return openssl_encrypt ($valor, $method, $clave, false, $iv);
  };
 
  //Desencripta
- $decrypt = function ($valor) use ($method, $clave, $iv) {
+ $decrypt = function ($valor, $method, $clave, $iv) {
      $encrypted_data = base64_decode($valor);
      return openssl_decrypt($valor, $method, $clave, false, $iv);
  };
