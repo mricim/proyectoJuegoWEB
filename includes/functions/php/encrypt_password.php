36e70cb8818c5376f41388580755521a9485976e
Monica <monicasegarra3@gmail.com>
2020-06-09 23:34:04 +0200

    Revert "Noves funcions d'encriptació (encrypt_password.php)"
    This reverts commit 177a41d7

--- includes/functions/php/encrypt_password.php
+++ includes/functions/php/encrypt_password.php
@@ -2,22 +2,8 @@
 <?php
 
  //Encripta
- /*function encrypt ($valor) {
-
+ function encrypt ($valor) {
      return hash('md5', $valor);
- };*/
-
- function encrypt($data) {
-     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-     $encrypted=openssl_encrypt($data, "aes-256-cbc", $key, 0, $iv);
-     return base64_encode($encrypted."::".$iv);
- }
-
- function decrypt($data) {
-     list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
-     return openssl_decrypt($encrypted_data, 'aes-256-cbc', $key, 0, $iv);
- }
-
-$key="1235@";
+ };
 
 ?>
