cc0b801020518467e4307f0ec2239ce5225d22ff
Monica <monicasegarra3@gmail.com>
2020-06-10 00:21:09 +0200

    Modificació funció 'encrypt'

--- example/users/insertUser.php
+++ example/users/insertUser.php
@@ -48,7 +48,7 @@ $links = conectar_db();
             	echo "<br>No se a enviado el mensaje";
             	//echo "<script> window.location='../../../example/users/registerdone.html?AddedUser=error'; </script>";
             } else {
-                echo "Result mensaje: ".$result_mensaje."<br>";
+                echo "<br>Result mensaje: ".$result_mensaje."<br>";
             	echo"<br>Se a guardado en nuestra base de datos<br>";
             	echo"<br><b>Todo correcto</b><br>";
             }
--- includes/functions/php/encrypt_password.php
+++ includes/functions/php/encrypt_password.php
@@ -2,8 +2,20 @@
 <?php
 
  //Encripta
- function encrypt ($valor) {
+ /*function encrypt ($valor) {
      return hash('md5', $valor);
- };
+ };*/
+
+ function encrypt($cadena){
+     $key='edu766648';  // Una clave de codificacion, debe usarse la misma para encriptar y desencriptar
+     $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $cadena, MCRYPT_MODE_CBC, md5(md5($key))));
+     return $encrypted; //Devuelve el string encriptado
+ }
+
+ function desencriptar($cadena){
+      $key='edu766648';  // Una clave de codificacion, debe usarse la misma para encriptar y desencriptar
+      $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($cadena), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+     return $decrypted;  //Devuelve el string desencriptado
+ }
 
 ?>
