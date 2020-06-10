5f69e73055809e53e72ef27906048c827cd5ec36
Monica <monicasegarra3@gmail.com>
2020-06-10 02:38:58 +0200

    Arreglo users_functions.js

--- includes/js/users_functions.js
+++ includes/js/users_functions.js
@@ -1,4 +1,30 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+})();
 
 var mailExists = false;
 $.ajax({
