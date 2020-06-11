15da60c7b9cfad89198919f8890cc63af69f1980
Monica <monicasegarra3@gmail.com>
2020-06-11 05:17:03 +0200

    conexion a la bd desde user_exists.php

--- includes/functions/php/user_exists.php
+++ includes/functions/php/user_exists.php
@@ -6,8 +6,29 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/functions/php/encrypt.php');
-include_once($_SERVER['DOCUMENT_ROOT'] .'/includes/functions/php/db_connection.php');
 
+function conectar_db(){
+		// conexion a Servidor
+		$host = "localhost";#nombre del host
+		$user = "user";#Usuario
+		$pass = "user12345!Ab";#contraseña
+		$db_name = "armegis";#Nombre de la base de datos
+		$conexion = mysqli_connect($host, $user, $pass);#Envio a la base de datos (Todas las variables antes creadas)
+		$conexion->query("SET NAMES 'utf8'");#Le decimos en que charset hay que conectarse
+
+		if( !$conexion ){ //Si la conexión no se ha logrado
+			exit;
+		} else {
+			//seleccion de base de datos(Aqui ya se a establecido la conecxion continua) y iniciamos a decirle a que base de datos se tiene que conectar
+			$select_db = mysqli_select_db($conexion,$db_name);
+			if( !$select_db ){#La base de datos no existe o hay algun problema
+				exit;
+			}
+			else{#TODO CORRECTO
+				return $conexion;
+			}
+		}
+	}
 
 
     $loginMail = encrypt($_POST['m']);
