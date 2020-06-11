afa7dc5350f946f0ee1fbc74f1b3cd6205af9d76
Monica <monicasegarra3@gmail.com>
2020-06-11 05:25:00 +0200

    eliminación includes en insertUser.php

--- example/users/insertUser.php
+++ example/users/insertUser.php
@@ -7,8 +7,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once($_SERVER['DOCUMENT_ROOT'] .'/includes/functions/php/db_connection.php');
-include($_SERVER['DOCUMENT_ROOT'] . '/includes/functions/php/encrypt.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/functions/php/user_exists.php');
 
 $links = conectar_db();
