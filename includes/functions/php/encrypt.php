58b5c8707c64b97b5f5dddb5d97be1024679d482
MrICIM <ericcasanova.m@gmail.com>
2020-06-12 21:28:31 +0200

    "AES" to "aes"

--- includes/functions/php/encrypt.php
+++ includes/functions/php/encrypt.php
@@ -8,13 +8,13 @@
 
 
     function encrypt($dato) {
-        $password='password';
-        return openssl_encrypt($dato,"AES-256-cbc",$password,OPENSSL_RAW_DATA,"iv");
+        $key='password';
+        return openssl_encrypt($dato,"aes-256-cbc",$key,OPENSSL_RAW_DATA,"iv");
     }
 
     function decrypt($dato) {
-        $password='password';
-        return openssl_decrypt($dato,"AES-256-cbc",$password,OPENSSL_RAW_DATA,"iv");
+        $key='password';
+        return openssl_decrypt($dato,"aes-256-cbc",$key,OPENSSL_RAW_DATA,"iv");
     }
 
     function encryptPassword($pass,$mail,$user) {
