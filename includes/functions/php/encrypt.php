619ddfb78f64f42e150621874dcee4c761a3f3d6
MrICIM <ericcasanova.m@gmail.com>
2020-06-12 21:21:36 +0200

    encrypt.php "aes-256-cbc"

--- includes/functions/php/encrypt.php
+++ includes/functions/php/encrypt.php
@@ -9,12 +9,12 @@
 
     function encrypt($dato) {
         $password='password';
-        return openssl_encrypt($dato,"AES-128-ECB",$password);
+        return openssl_encrypt($dato,"AES-256-cbc",$password,OPENSSL_RAW_DATA,"iv");
     }
 
     function decrypt($dato) {
         $password='password';
-        return openssl_decrypt($dato,"AES-128-ECB",$password);
+        return openssl_decrypt($dato,"AES-256-cbc",$password,OPENSSL_RAW_DATA,,"iv");
     }
 
     function encryptPassword($pass,$mail,$user) {
