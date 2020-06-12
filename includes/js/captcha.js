d0736794378c95339d0e36af49a995bd8f5eb093
Monica <monicasegarra3@gmail.com>
2020-06-12 17:15:42 +0200

    Arreglo función recaptcha en el registro y listar versiones Windows

--- includes/js/captcha.js
+++ includes/js/captcha.js
@@ -1,4 +1,4 @@
-function Captcha(mainCaptcha) {
+function Captcha() {
     var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
     var i;
     for (i = 0; i < 6; i++) {
