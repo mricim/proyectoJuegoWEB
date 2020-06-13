96bf96ed84fb731b63c08b0d3f65ffcc459687f8
MrICIM <ericcasanova.m@gmail.com>
2020-06-13 10:02:10 +0200

    OPENSSL_RAW_DATA test 2

--- includes/functions/php/encrypt.php
+++ includes/functions/php/encrypt.php
@@ -6,20 +6,37 @@
      return hash('md5', $valor);
  }*/
 
+function encrypt($data) {
+$key='0123456789abcdef';
+$iv='aaaaaaaaaaaaaaaa';
+        if (strlen($key) < PHP_AES_Cipher::$CIPHER_KEY_LEN) {
+            $key = str_pad("$key", PHP_AES_Cipher::$CIPHER_KEY_LEN, "0"); //0 pad to len 16
+        } else if (strlen($key) > PHP_AES_Cipher::$CIPHER_KEY_LEN) {
+            $key = substr($str, 0, PHP_AES_Cipher::$CIPHER_KEY_LEN); //truncate to 16 bytes
+        }
 
+        $encodedEncryptedData = base64_encode(openssl_encrypt($data, PHP_AES_Cipher::$OPENSSL_CIPHER_NAME, $key, OPENSSL_RAW_DATA, $iv));
+        $encodedIV = base64_encode($iv);
+        $encryptedPayload = $encodedEncryptedData.":".$encodedIV;
+
+        return $encryptedPayload;
+
+    }
+/*
     function encrypt($dato) {
         $key='0123456789abcdef';
         //return openssl_encrypt($dato,"aes-256-cbc",$key);
         $iv='aaaaaaaaaaaaaaaa';
-        return openssl_encrypt($dato,"aes-128-cbc",$key,OPENSSL_RAW_DATA,$iv);
         //return base64_encode(openssl_encrypt($dato, "aes-256-cbc", AesCipher::fixKey($key), OPENSSL_RAW_DATA, $iv));
+        //return openssl_encrypt($dato,"aes-128-cbc",$key,0,$iv);
+        return openssl_encrypt($dato,"aes-128-cbc",$key,0,$iv);
     }
-
+*/
     function decrypt($dato) {
         $key='0123456789abcdef';
         //return openssl_decrypt($dato,"aes-256-cbc",$key);
         $iv='aaaaaaaaaaaaaaaa';
-        return openssl_decrypt($dato,"aes-128-cbc",$key,OPENSSL_RAW_DATA,$iv);
+        return openssl_decrypt($dato,"aes-128-cbc",$key,0,$iv);
     }
 
     function encryptPassword($pass,$mail,$user) {
