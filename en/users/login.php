08247131b1111831f52273c18968c02411b7b57c
Monica <monicasegarra3@gmail.com>
2020-06-18 10:49:35 +0200

    sessio

--- en/users/login.php
+++ en/users/login.php
@@ -18,26 +18,25 @@ echo "Mail després: ".encrypt($_POST['loginMail'];
 
         //Si existe el usuario -> return 0
         //Si no existe el usuario -> return 1
-		function userExists($mail) {
-		    $conectar= conectar_db();
+	//	function userExists($mail) {
+	//	    $conectar= conectar_db();
            // echo '<br>Mail: '.$mail.'<br>';
-		    $consulta = "SELECT * FROM users WHERE email = '$mail'";
-            $resultado = mysqli_query($conectar, $consulta);
+	//	    $consulta = "SELECT * FROM users WHERE email = '$mail'";
+      //      $resultado = mysqli_query($conectar, $consulta);
 
-            while($unrow = mysqli_fetch_array($resultado)){
-            	$array_resultado[] = $unrow;
-            }
-echo $loginMail;
-            if (count($array_resultado) > 0) {
+        //    while($unrow = mysqli_fetch_array($resultado)){
+            //	$array_resultado[] = $unrow;
+          //  }
+//            if (count($array_resultado) > 0) {
                // echo '<br>El usuario con email = '. $mail . 'ya existe.';
-               mysqli_close($conectar);
+  //             mysqli_close($conectar);
                //echo "<script> window.location='../../../en/users/userlogin.html?Login=true&User=".$loginMail.'; </script>";
 
-            } else {
-                mysqli_close($conectar);
+    //        } else {
+      //          mysqli_close($conectar);
                 //echo "<script> window.location='../../../en/users/userlogin.html?Login=false&User=".$loginMail."'; </script>";
-            }
-		}
+        //    }
+		//}
 
 
 ?>
